709f32e94a53c84b8515c979d0ba290d7041fd53
mallikagogoi7 <mallikagogoi7@gmail.com>
2023-06-21 13:16:45 +0530

    boiler plate changes

---
+++ data-prepper-plugins/http-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/HttpSinkWorker.java
@@ -0,0 +1,21 @@
+package org.opensearch.dataprepper.plugins.sink;
+
+public class HttpSinkWorker implements Runnable {
+
+    private final HttpAuthOptions httpEndPointDetails;
+
+    public HttpSinkWorker(final HttpAuthOptions httpEndPointDetails){
+        this.httpEndPointDetails = httpEndPointDetails;
+    }
+
+    @Override
+    public void run() {
+        try{
+            // push to http end point based on workers - Callable<>
+        }catch(Exception e){
+            // In case of any exception, need to write the exception in dlq  - logFailureForDlqObjects();
+            // In case of any exception, need to push the web hook url- logFailureForWebHook();
+        }
+        //TODO: implement end to end acknowledgement
+    }
+}
